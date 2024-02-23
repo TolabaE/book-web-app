2ce46beb80555a7feb775fc0e550c83b943dc05e
Eduardo Tolaba <eduardoetolaba@gmail.com>
2024-02-23 15:38:40 -0300

    arreglos en el componente carrusel, agregado de la carpeta backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Home from './pages/home/Home';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
 import Contact from './pages/Contact/Contact';
+import About from './pages/About/About';
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/contacto' element={<Contact/>} />
+          <Route path='/nosotros' element={<About/>} />
         </Routes>
       <Footer/>
     </BrowserRouter>
---
+++ frontend/src/pages/About/About.tsx
@@ -0,0 +1,14 @@
+import './about.css';
+
+
+const About = () => {
+
+
+    return (
+        <div>
+            Hola soy la seccion nosotros
+        </div>
+    )
+}
+
+export default About;
